5ba989f66df2639671ef3ca5460dc38212bb16f6
SakifKhan98 <sakifkhan98@gmail.com>
2024-11-10 19:53:22 -0600

    Changed app version

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->List[str]:
-  '''
-  Read the requirements file and return the list of requirements
-  '''
-  requirements = []
-  with open(file_path) as file_obj:
-    requirements = file_obj.readlines()
-    requirements = [req.replace('\n','') for req in requirements]
-    if HYPHEN_E_DOT in requirements:
-      requirements.remove(HYPHEN_E_DOT)
-  return requirements
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Read the requirements file and return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
 
 setup(
-    name='student-performance-prediction',
-    version='0.0.1',
-    author='Sakif Khan',
-    author_email= 'sakifkhan98@gmail.com',
+    name="student-performance-prediction",
+    version="1.12.3",
+    author="Sakif Khan",
+    author_email="sakifkhan98@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt'),
+    install_requires=get_requirements("requirements.txt"),
 )
